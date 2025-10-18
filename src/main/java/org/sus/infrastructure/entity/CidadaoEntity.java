e7cb248cc6a04f7389981606689344e7c5f9ef42
igorj <igor.castro_ext@light.com.br>
2025-10-18 19:45:28 -0300

    Ajuste do cidadao

--- src/main/java/org/sus/infrastructure/entity/CidadaoEntity.java
+++ src/main/java/org/sus/infrastructure/entity/CidadaoEntity.java
@@ -6,10 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serial;
+import java.util.Objects;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "cidadao")
 public class CidadaoEntity {
@@ -23,4 +21,59 @@ public class CidadaoEntity {
     private String nome;
     private String cpf;
     private String email;
+
+    public CidadaoEntity(Long id, String email, String cpf, String nome) {
+        this.id = id;
+        this.email = email;
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    public CidadaoEntity() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CidadaoEntity that = (CidadaoEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(nome, that.nome) && Objects.equals(cpf, that.cpf) && Objects.equals(email, that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, cpf, email);
+    }
 }
